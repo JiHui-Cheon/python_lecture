8f9ec954294db4430020755139bd4322accdc98c
cjh <cjh@example.com>
2021-04-15 17:35:16 +0900

    반복문 제어까지

--- skip_vowel_continue.py
+++ skip_vowel_continue.py
@@ -1,8 +1,8 @@
 st='Programming'
 
 for ch in st:
-    if ch in ['a','e', 'i', 'o', 'u']
+    if ch in ['a','e', 'i', 'o', 'u']:
         continue
     print(ch)
 
-print ('The end')
+print('The end')
--- while_sum_input.py
+++ while_sum_input.py
@@ -1,7 +1,7 @@
-n = int(input('합계를 구할 양의 정수를 이 ㅂ력하세요:'))
+n = int(input('합계를 구할 양의 정수를 입력하세요:'))
 s = 0
+i = 1
 
-for i in range(1, n+1):
+while i<=n
     s = s + i
-
 print('1부터 {}까지의 합은 {}'.format(n,s))
